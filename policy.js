a13734280d6431081d7aaa8141f3562774ea307b
sreeharsha2736 <sreeharsha2736@github.com>
2021-02-08 16:00:56 +0530

    update

--- policy.js
+++ policy.js
@@ -2,63 +2,61 @@ console.clear();
 
 // (Action Creators)
 const createPolicy = (name, amount) => {
-  return { // Action (a form in our analogy)
-    type: 'CREATE_POLICY',
+  return {
+    // Action (a form in our analogy)
+    type: "CREATE_POLICY",
     payload: {
       name: name,
-      amount: amount
-    }
+      amount: amount,
+    },
   };
 };
 
 const deletePolicy = (name) => {
   return {
-    type: 'DELETE_POLICY',
+    type: "DELETE_POLICY",
     payload: {
-      name: name
-    }
+      name: name,
+    },
   };
 };
 
 const createClaim = (name, amountOfMoneyToCollect) => {
   return {
-    type: 'CREATE_CLAIM',
+    type: "CREATE_CLAIM",
     payload: {
       name: name,
-      amountOfMoneyToCollect: amountOfMoneyToCollect
-    }
+      amountOfMoneyToCollect: amountOfMoneyToCollect,
+    },
   };
 };
 
-
 // Reducers
 const claimsHistory = (oldListOfClaims = [], action) => {
-  if (action.type === 'CREATE_CLAIM') {
-    // we care about this action (FORM!)
+  if (action.type === "CREATE_CLAIM") {
     return [...oldListOfClaims, action.payload];
   }
-  
 
   return oldListOfClaims;
 };
 
 const accounting = (bagOfMoney = 100, action) => {
-  if (action.type === 'CREATE_CLAIM') {
+  if (action.type === "CREATE_CLAIM") {
     return bagOfMoney - action.payload.amountOfMoneyToCollect;
-  } else if (action.type === 'CREATE_POLICY') {
+  } else if (action.type === "CREATE_POLICY") {
     return bagOfMoney + action.payload.amount;
   }
-  
+
   return bagOfMoney;
 };
 
 const policies = (listOfPolicies = [], action) => {
-  if (action.type === 'CREATE_POLICY') {
+  if (action.type === "CREATE_POLICY") {
     return [...listOfPolicies, action.payload.name];
-  } else if (action.type === 'DELETE_POLICY') {
-    return listOfPolicies.filter(name => name !== action.payload.name);
+  } else if (action.type === "DELETE_POLICY") {
+    return listOfPolicies.filter((name) => name !== action.payload.name);
   }
-  
+
   return listOfPolicies;
 };
 
@@ -67,19 +65,17 @@ const { createStore, combineReducers } = Redux;
 const ourDepartments = combineReducers({
   accounting: accounting,
   claimsHistory: claimsHistory,
-  policies: policies
+  policies: policies,
 });
 
 const store = createStore(ourDepartments);
 
-createPolicy('Alex', 20)
-createClaim('Alex', 120)
-deletePolicy('Alex')
+createPolicy("Alex", 20);
+createClaim("Alex", 120);
+deletePolicy("Alex");
 
-store.dispatch(createPolicy('Alex', 20));
-store.dispatch(createPolicy('Jim', 30));
-store.dispatch(createPolicy('Bob', 40));
-
-
+store.dispatch(createPolicy("Alex", 20));
+store.dispatch(createPolicy("Jim", 30));
+store.dispatch(createPolicy("Bob", 40));
 
 console.log(store.getState());
